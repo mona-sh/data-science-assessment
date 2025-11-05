2f31b9530c1479050966a6a64f4204641e59e485
mona-sh <152225521+mona-sh@users.noreply.github.com>
2025-11-05 22:16:38 +0300

    Update answer.sql

--- answer.sql
+++ answer.sql
@@ -1,1 +1,14 @@
--- ADD YOUR SQL QUERY HERE
+SELECT 
+    it.investor_id,
+    s.sector_name,
+    SUM(it.no_of_shares) AS sector_shares,
+    ROUND(
+        SUM(it.no_of_shares) * 100.0 /
+        (SELECT SUM(no_of_shares)
+         FROM investor_transactions t
+         WHERE t.investor_id = it.investor_id),
+        2
+    ) AS share_percentage
+FROM investor_transactions it
+JOIN sectors s ON it.sector_id = s.sector_id
+GROUP BY it.investor_id, s.sector_name;
